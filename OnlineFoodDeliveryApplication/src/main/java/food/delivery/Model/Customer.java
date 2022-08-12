9c7a6fd28039a3fd63d02cd6db809b2ca1bf78d9
Tanish Mangal <mangaltanish103@gmail.com>
2022-08-12 18:25:37 +0800

    customerClass addes

--- OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Customer.java
+++ OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Customer.java
@@ -1,7 +1,10 @@
 package food.delivery.Model;
 
 import javax.persistence.*;
+import javax.validation.*;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.*;
 
@@ -13,9 +16,13 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String customerId;
+	
 	@NonNull
+	@Pattern(regexp="[a-z]{3,12}", message="Last must Not Contain Special Character")
 	private String firstName;
+	
 	@NonNull
+	@Pattern(regexp="[a-z]{3,12}", message="Last must Not Contain Special Character")
 	private String lastName;
 	
 	@NonNull
@@ -24,7 +31,12 @@ public class Customer {
 	
 	@NonNull
 	private String gender;
+	@NonNull
+	@Size(min=10 ,max=10,message="Mobile Must have 10 Digit")
+	@Pattern(regexp="[6789][0-9]{9}",message="Please Enter Valid Mobile No")
 	private String mobileNumber;
+	
+	@NonNull
 	private Address address;
 
 }
