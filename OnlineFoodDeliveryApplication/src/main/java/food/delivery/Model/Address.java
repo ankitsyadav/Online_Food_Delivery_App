ed270ad9d52153a70ce55401e30662876f5ae486
Niraj Kumar <n1r4jkumar@gmail.com>
2022-08-12 14:33:01 +0530

    address added

--- OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Address.java
+++ OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Address.java
@@ -1,5 +1,16 @@
 package food.delivery.Model;
 
 public class Address {
-
+	
+	private String addressId;
+	private String buildingName;
+	private String streetNo;
+	private String area;
+	private String city;
+	private String state;
+	private String country;
+	private String pincode;
+	
+	
+	
 }
