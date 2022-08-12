008fb4ba91db6aa0e9fc7d04841978f1aa74d875
Tanish Mangal <mangaltanish103@gmail.com>
2022-08-12 20:37:11 +0800

    category class update

--- OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Category.java
+++ OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/Category.java
@@ -1,7 +1,20 @@
 package food.delivery.Model;
 
+import javax.persistence.*;
+import javax.validation.constraints.*;
 
+import lombok.*;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Category {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	String catId;
+	@NotNull(message = "category should not be null")
+	String categoryName;
 }
