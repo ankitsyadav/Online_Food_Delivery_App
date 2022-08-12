2835e494daa72efb1f8d3495cc6a4764886ce998
ANKIT SINGH YADAV <95612855+ankitsyadav@users.noreply.github.com>
2022-08-12 15:59:33 +0530

    Model class FoodCart,Bill,Orderdetals created with all annotations

---
+++ OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/FoodCart.java
@@ -0,0 +1,32 @@
+package food.delivery.Model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class FoodCart {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public String cartId;
+	@NotNull(message = "customer shoud not be null")
+	private Customer customer;
+	@NotNull(message = "itemList shoud not be null")
+	private List<Item> itemList;
+
+}
---
+++ OnlineFoodDeliveryApplication/src/main/java/food/delivery/Model/OrderDetails.java
@@ -0,0 +1,32 @@
+package food.delivery.Model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class OrderDetails {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer orderId;
+	@NotNull(message = "order Date shoud not be null")
+	private LocalDateTime orderDate;
+	@NotNull(message = "cart shoud not be null")
+	private FoodCart cart;
+
+}
